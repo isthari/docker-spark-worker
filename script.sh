44352b9e2cf729fd13331e944132d11dec3dcef3
jhernan <jose.hernandez@isthari.com>
2016-12-16 22:25:11 +0100

    added v2.0.0

--- script.sh
+++ script.sh
@@ -4,6 +4,12 @@ docker build -t isthari/spark-worker:1.6.2 .
 docker push isthari/spark-worker:1.6.2
 cd ..
 
+echo "2.0.0"
+cd 2.0.0
+docker build -t isthari/spark-worker:2.0.0 .
+docker push isthari/spark-worker:2.0.0
+cd ..
+
 echo "2.0.1"
 cd 2.0.1
 docker build -t isthari/spark-worker:2.0.1 .
