5fbfeb9bfca1aba7da59fd0e46fd01e4567a617d
jhernan <jose.hernandez@isthari.com>
2016-12-16 22:11:09 +0100

    multitag

---
+++ script.sh
@@ -0,0 +1,13 @@
+echo "1.6.2"
+cd 1.6.2
+docker build -t isthari/spark-worker:1.6.2 .
+docker push isthari/spark-worker:1.6.2
+cd ..
+
+echo "2.0.1"
+cd 2.0.1
+docker build -t isthari/spark-worker:2.0.1 .
+docker push isthari/spark-worker:2.0.1
+cd ..
+
+
